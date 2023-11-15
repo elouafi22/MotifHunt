70a6905f7b93c82e7e81e802138c377cfbbd7c8d
elouafi22 <elouafimed2@gmail.com>
2023-11-16 00:52:43 +0100

    some modification

--- apiRest/api.py
+++ apiRest/api.py
@@ -4,7 +4,7 @@ from flask import jsonify
 app = flask.Flask(__name__)
 
 
-@app.route("/text", methods=["POST"])
+@app.route("/text", methods=["GET"])
 def get_text():
     text = "bonjour tout le monde"
     return jsonify({"text": text})
