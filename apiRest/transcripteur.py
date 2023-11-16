ec5d047ae79632693315cc9f1099f4e7558d1965
elouafi22 <elouafimed2@gmail.com>
2023-11-16 01:32:07 +0100

    some modification

--- apiRest/transcripteur.py
+++ apiRest/transcripteur.py
@@ -4,10 +4,14 @@ import whisper
 class transcription:
     "this class for trancripe audio to text"
 
-    def __init__(self, nomAudio="/content/audio.mp3", Namemodel="base"):
+    def __init__(self, nomAudio="../resource/audio.mp3", Namemodel="base"):
         self.nomAudio = nomAudio
         self.model = whisper.load_model(Namemodel)
 
     def execute(self):
         result = self.model.transcribe(self.nomAudio)
         return result["text"]
+
+
+obj = transcription()
+obj.execute()
